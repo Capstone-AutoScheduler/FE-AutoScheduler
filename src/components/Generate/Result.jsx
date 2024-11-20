2a4c1488b6ce901380a6ac03b750274fc4ef5c2a
bbakbum <zxc534@naver.com>
2024-11-20 23:19:20 +0900

    [feat] change mechanism, implement select action with drag area

--- src/components/Generate/Result.jsx
+++ src/components/Generate/Result.jsx
@@ -4,7 +4,7 @@ import { styled } from 'styled-components';
 import useGenerateStore from '../../store/GenerateStore';
 
 const Result = () => {
-    const { pdfContent } = useGenerateStore(state=>state);
+    const { numPages, targetPage, increaseTargetPage, pdfContent } = useGenerateStore(state=>state);
     
     const frames = JSON.parse(localStorage.getItem('frames'));
     const mapping = JSON.parse(localStorage.getItem('mapping'));
@@ -14,40 +14,74 @@ const Result = () => {
         //traverse content
         //find mapping on content
         //get offset
-        var offset = 0;
-        const target = mapping[0].string;
-        pdfContent.forEach((item, index) => {
-            //console.log(item.str);
-            if(item.str === target) {
-                //console.log('found mapping!');
-                console.log('mapping index : ', mapping[0].bubbleId);
-                console.log('content index : ', index);
+        var offsetX = 0;
+        var offsetY = 0;
+        var found = false;
+        pdfContent.forEach((item) => {
+            if(item.str === mapping[0].string) {
+                console.log('found mapping!');
+                //console.log('mapping : ', mapping[0]);
+                //console.log('found content : ', item);
                 //index(of frame) + offset ==> actual index (of content)
-                offset = index - mapping[0].bubbleId;
-                console.log('offset : ', offset);
+                offsetX = item.x - mapping[0].x;
+                offsetY = item.y - mapping[0].y;
+                console.log('offsetX : ', offsetX);
+                console.log('offsetY : ', offsetY);
+                
+                found = true;
             }
         });
+        if (found === false) {
+            console.log('target page : ', targetPage);
+            console.log('failed to map');
+            if(targetPage < numPages) {
+                increaseTargetPage();
+                console.log('increase target page');
+            }
+            return; 
+        } else {
+            console.log('target page : ', targetPage);
+            console.log('found map');
+            console.log('start generating');
+        }
         //
         //traverse frames
         //generate schedule using offset
-        
         console.log('content : ', pdfContent);
         //console.log('frames : ', frames);
         //console.log('frame.title : ', pdfContent[frames[0].title[0].startBubbleId + offset].str);
-        frames.forEach((frame)=>{
+        frames.forEach((frame, index)=>{
+            console.log('### FRAME ' + index + '###');
             console.log('title');
             frame.title.forEach((item) => {
-                console.log(pdfContent[item.startBubbleId + offset].str);
+                console.log(findItemInArea(item.area, offsetX, offsetY));
             })
             console.log('date');
             frame.date.forEach((item) => {
-                console.log(pdfContent[item.startBubbleId + offset].str);
+                console.log(findItemInArea(item.area, offsetX, offsetY));
             })
             console.log('detail');
             frame.detail.forEach((item) => {
-                console.log(pdfContent[item.startBubbleId + offset].str);
+                console.log(findItemInArea(item.area, offsetX, offsetY));
             })
         });
+    }
+
+    const findItemInArea = (area, offsetX, offsetY) => {
+        const items = [];
+        const start = {x: area.start.x + offsetX, y: area.start.y + offsetY};
+        const end = {x: area.end.x + offsetX, y: area.end.y + offsetY};
+
+        //console.log('area start', start);
+        //console.log('area end', end);
+        pdfContent.forEach((item) => {
+            if (((start.x <= item.x)&&(item.x<=end.x)) && ((start.y <= item.y)&&(item.y <= end.y))) {
+                //console.log('inside', item.str);
+                items.push(item.str);
+            }
+        })
+
+        return items;
     }
 
     useEffect(() => {
--- src/store/GenerateStore.jsx
+++ src/store/GenerateStore.jsx
@@ -1,6 +1,16 @@
 import { create } from "zustand";
 
 const useGenerateStore = create((set) => ({
+    numPages: 0,
+    setNumPages: (num) => set({
+        numPages: num,
+    }),
+    targetPage: 1,
+    setTargetPage: (num) => set({
+        targetPage: num,
+    }),
+    increaseTargetPage: () => set((state) => ({ targetPage: state.targetPage + 1 })),
+
     pdfContent: null,
     setPdfContent: (content) => set({
         pdfContent: content,
