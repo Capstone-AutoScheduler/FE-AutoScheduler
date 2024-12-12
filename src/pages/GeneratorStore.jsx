2843eda928475769d7b7e3d0f425b5505a523b75
Jinho622 <aceho21@gmail.com>
2024-12-12 21:19:47 +0900

    feat(): 상점 페이지 전체 생성기 조회 api 연결

--- src/pages/GeneratorStore.jsx
+++ src/pages/GeneratorStore.jsx
@@ -1,44 +1,80 @@
 import styled from "styled-components";
 import React, { useState, useCallback, useEffect } from "react"; // useState, useCallback 추가
-
-const data = [
-  {
-    title: "중앙대학교 강의계획서",
-    description: "강의계획서에 대한 생성기입니다.",
-  },
-  {
-    title: "서울대학교 강의계획서",
-    description: "강의계획서에 대한 생성기입니다.",
-  },
-  {
-    title: "연세대학교 강의계획서",
-    description: "강의계획서에 대한 생성기입니다.",
-  },
-  {
-    title: "고려대학교 강의계획서",
-    description: "강의계획서에 대한 생성기입니다.",
-  },
-  {
-    title: "한양대학교 강의계획서",
-    description: "강의계획서에 대한 생성기입니다.",
-  },
-  {
-    title: "성균관대학교 강의계획서",
-    description: "강의계획서에 대한 생성기입니다.",
-  },
-];
-
+import axios from "axios";
 const GeneratorStore = () => {
+  const [list, setList] = useState([]);
+  const [openCreate, setOpenCreate] = useState(false);
+  const [typeColor, setTypeColor] = useState("#4caf50");
+
+  async function getAllGeneratorList() {
+    try {
+      const response = await axios.get(
+        `http://3.35.252.162:8080/generator/list`,
+        {
+          timeout: 3000,
+        }
+      );
+      console.log(response.data.result.generators);
+      setList(response.data.result.generators);
+    } catch (error) {
+      console.error("Failed to get generator list", error);
+    }
+  }
+
+  useEffect(() => {
+    getAllGeneratorList();
+  }, []);
+
   return (
     <div>
-      <Name>생성기 Store</Name>
       <Container>
-        {data.map((item, index) => (
-          <Generator>
-            <Title>{item.title}</Title>
-            <Description>{item.description}</Description>
-          </Generator>
-        ))}
+        <GenerateContainer>
+          {list.map((item, index) => (
+            <Generator>
+              <Info>
+                <Type>
+                  <TypeContent>
+                    <div
+                      style={{
+                        marginRight: "4px",
+                        backgroundColor:
+                          item.sourceType === "PDF" ? "#b30c00" : "#4caf50",
+                        width: "15px",
+                        height: "15px",
+                        borderRadius: "15px",
+                      }}
+                    ></div>
+                    <div
+                      style={{
+                        color:
+                          item.sourceType === "PDF" ? "#b30c00" : "#4caf50",
+                      }}
+                    >
+                      {item.sourceType}
+                    </div>
+                  </TypeContent>
+                </Type>
+                <Creator>
+                  <CreateContent>
+                    <div
+                      style={{
+                        marginRight: "4px",
+                        backgroundColor: "#008bf0",
+                        width: "15px",
+                        height: "15px",
+                        borderRadius: "15px",
+                      }}
+                    ></div>
+                    <div>{item.memberName}</div>
+                  </CreateContent>
+                </Creator>
+                <Icon></Icon>
+              </Info>
+              <Title>{item.generatorTitle}</Title>
+              <Description>{item.generatorDetail}</Description>
+            </Generator>
+          ))}
+        </GenerateContainer>
       </Container>
     </div>
   );
@@ -52,26 +88,78 @@ const GeneratorStore = () => {
 // -> 상단메뉴에대한 css 찾아보기
 // 네이버는 head의 css파일만으로 css전부다 적용됨.
 
-const Name = styled.div`
-  padding-left: 80px;
-  padding-top: 80px;
-  font-size: 30px;
+const Info = styled.div`
+  display: flex;
+`;
+
+const Creator = styled.div`
+  display: flex;
+`;
+
+const CreateContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 4px;
+  padding: 0px 4px;
+  color: #008bf0;
+  background-color: #ffffff;
+  border-radius: 4px;
+`;
+
+const Type = styled.div`
+  display: flex;
+`;
+
+const TypeContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 4px;
+  padding: 0px 4px;
+  background-color: #ffffff;
+  border-radius: 4px;
 `;
 
 const Title = styled.div`
   font-size: 19px;
   margin-bottom: 6px;
+  padding-left: 6px;
+  padding-top: 10px;
 `;
 
 const Description = styled.div`
+  display: -webkit-box; /* 플렉스박스를 사용한 레이아웃 */
+  -webkit-box-orient: vertical; /* 텍스트를 세로 방향으로 정렬 */
+  -webkit-line-clamp: 4; /* 최대 줄 수를 3줄로 제한 */
+  overflow: hidden; /* 넘치는 텍스트 숨기기 */
+  text-overflow: ellipsis;
   font-size: 13px;
+  padding-left: 6px;
+`;
+
+const Icon = styled.div`
+  padding: 20px;
 `;
 
 const Container = styled.div`
+  padding: 20px;
+`;
+
+const GenerateContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 200px);
-  gap: 40px 80px;
-  padding: 80px;
+  grid-template-rows: repeat(3, 150px);
+  row-gap: 40px; /* 세로 간격 */
+  column-gap: 80px; /* 가로 간격 */
+  background-color: #ffffff;
+
+  padding-top: 40px;
+  padding-bottom: 60px;
+  padding-left: 49px;
+
+  border-radius: 20px;
+  box-shadow: 0px 0px 10px 0px var(--Gray-800, #dedede);
 
   @media (max-width: 768px) {
     grid-template-columns: repeat(2, 1fr);
@@ -79,13 +167,13 @@ const Container = styled.div`
 `;
 
 const Generator = styled.div`
-  width: 200px;
-  height: 120px;
-  background-color: #f4f4f4;
-  border: 1px solid #ccc;
+  width: 260px;
+  height: 170px;
+  background-color: rgba(255, 255, 100, 0.6);
+  border: 2px solid rgba(255, 204, 0, 1);
   border-radius: 8px;
-  padding: 20px;
-  text-align: center;
+  padding: 8px;
+  padding-top: 12px;
 `;
 
 export default GeneratorStore;
